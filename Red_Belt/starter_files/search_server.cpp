4d09900d932b39ef959e6eee638dbb42cf37ddd5
thepotterman <p-vov4ik@yandex.ru>
2020-08-19 10:20:46 +0300

    Passed!

--- Red_Belt/starter_files/search_server.cpp
+++ Red_Belt/starter_files/search_server.cpp
@@ -7,91 +7,90 @@
 #include <sstream>
 #include <iostream>
 
-using namespace std;
-
 vector<string> SplitIntoWords(const string& line) {
   istringstream words_input(line);
-  return {make_move_iterator(istream_iterator<string>(words_input)), make_move_iterator(istream_iterator<string>())};
+  return {istream_iterator<string>(words_input), istream_iterator<string>()};
 }
 
-InvertedIndex::InvertedIndex(istream & document_input) {
-    index.reserve(15000);
-    string line;
-    while(getline(document_input, line)) {
-        ++docs_count;
-        size_t id = docs_count - 1;
-        for(const string & word : SplitIntoWords(line)) {
-            auto & id_to_count = index[word];
-            if(!id_to_count.empty() && id_to_count.back().first == id) {
-                ++id_to_count.back().second;
-            } else {
-                id_to_count.emplace_back(id, 1);
-            }
-        }
-    }
-}
-
-void UpdateDocumentBaseSingleThread(istream& document_input, Synchronized<InvertedIndex> & _index) {
-  InvertedIndex new_index(document_input);
-
-  swap(_index.GetAccess().ref_to_value, new_index);
+SearchServer::SearchServer(istream& document_input) {
+  UpdateDocumentBase(document_input);
 }
 
 void SearchServer::UpdateDocumentBase(istream& document_input) {
-//    UpdateDocumentBaseSingleThread(document_input, index);
-    futures.push_back(async(UpdateDocumentBaseSingleThread, ref(document_input), ref(index)));
-}
+  InvertedIndex new_index;
+  
+  for (string current_document; getline(document_input, current_document); ) {
+    new_index.Add(move(current_document));
+  }
 
-void AddQueriesStreamSingleThread(
-  istream& query_input, ostream& search_results_output, Synchronized<InvertedIndex> & _index
-) {
-    vector<int64_t> ids;
-    vector<int64_t> ids_to_count;
-    string line;
-    while(getline(query_input, line)) {
-        const auto words = SplitIntoWords(line);
-        auto access = _index.GetAccess();
-        const size_t docs_count = access.ref_to_value.GetDocsCount();
-        ids_to_count.assign(docs_count, 0);
-        ids.resize(docs_count);
-
-        auto index = access.ref_to_value;
-
-        for(const auto & word : words) {
-            for(const auto & [doc_id, hit_count] : index.Lookup(word)) {
-                ids_to_count[doc_id] += hit_count;
-            }
-        }
-        iota(ids.begin(), ids.end(), 0);
-
-        partial_sort(ids.begin(), Head(ids, 5).end(), ids.end(),
-                [&ids_to_count](int64_t lhs, int64_t rhs){
-                    return pair(ids_to_count(lhs), -lhs) > pair(ids_to_count(rhs), -rhs);
-                });
-        search_result_output << line << ";";
-        
-        for(size_t doc_id : Head(ids, 5)) {
-            const size_t hit_count = ids_to_count[doc_id];
-            if(hit_count == 0) {
-                break;
-            }
-
-            search_result_output << " {docid: " << doc_id << ", hitcount: " << hit_count << "}";
-        }
-        search_result_output << "\n";
-    }
+  index = move(new_index);
 }
 
 void SearchServer::AddQueriesStream(
   istream& query_input, ostream& search_results_output
 ) {
-//    AddQueriesStreamSingleThread(query_input, search_results_output, index);
-    futures.push_back(async(AddQueriesStreamSingleThread, ref(query_input), ref(search_results_output), ref(index)));
+  for (string current_query; getline(query_input, current_query); ) {
+    const auto words = SplitIntoWords(current_query);
+
+//    map<size_t, size_t> docid_count;
+    vector<int64_t> ids(50000, 0);
+    vector<int64_t> ids_to_count(50000, 0);
+    iota(ids.begin(), ids.end(), 0);
+    for (const auto& word : words) {
+      for (auto & [id, count] : index.Lookup(word)) {
+        ids_to_count[id]+= count;
+      }
+    }
+
+//    vector<pair<size_t, size_t>> search_results(
+//      docid_count.begin(), docid_count.end()
+//    );
+/*    sort(
+      begin(search_results),
+      end(search_results),
+      [](pair<size_t, size_t> lhs, pair<size_t, size_t> rhs) {
+        int64_t lhs_docid = lhs.first;
+        auto lhs_hit_count = lhs.second;
+        int64_t rhs_docid = rhs.first;
+        auto rhs_hit_count = rhs.second;
+        return make_pair(lhs_hit_count, -lhs_docid) > make_pair(rhs_hit_count, -rhs_docid);
+      }
+    );
+*/
+    partial_sort(ids.begin(), Head(ids, 5).end(), ids.end(), 
+            [&ids_to_count](int64_t lhs, int64_t rhs) {
+                return make_pair(ids_to_count[lhs], -lhs) > make_pair(ids_to_count[rhs], -rhs); 
+            });
+    search_results_output << current_query << ':';
+//    for (auto [docid, hitcount] : Head(search_results, 5)) {
+    for (auto id : Head(ids, 5)) {
+      if(ids_to_count[id] == 0) {
+          break;
+      }
+      search_results_output << " {"
+        << "docid: " << id << ", "
+        << "hitcount: " << ids_to_count[id] << '}';
+    }
+    search_results_output << endl;
+  }
 }
 
+void InvertedIndex::Add(const string& document) {
+  docs.push_back(document);
 
-const vector<pair<size_t, size_t>> InvertedIndex::Lookup(const string& word) const {
-    static const vector<pair<size_t, size_t>> answer;
+  const size_t id = docs.size() - 1;
+  for (const auto& word : SplitIntoWords(document)) {
+      auto & id_to_count = index[word];
+      if(!id_to_count.empty() && id_to_count.back().first == id) {
+        ++id_to_count.back().second;
+      } else {
+        id_to_count.push_back({id, 1});
+      }
+  }
+}
+
+vector<pair<size_t, size_t>> InvertedIndex::Lookup(const string& word) const {
+    vector<pair<size_t, size_t>> answer;
   if (auto it = index.find(word); it != index.end()) {
     return it->second;
   } else {
--- Red_Belt/starter_files/search_server.h
+++ Red_Belt/starter_files/search_server.h
@@ -1,47 +1,34 @@
 #pragma once
 
-#include "synchronized.h"
-
 #include <istream>
 #include <ostream>
 #include <set>
 #include <vector>
 #include <map>
 #include <string>
-#include <future>
-
 using namespace std;
 
 class InvertedIndex {
 public:
-  InvertedIndex() = default;
+  void Add(const string& document);
+  vector<pair<size_t, size_t>> Lookup(const string& word) const;
 
-  explicit InvertedIndex(istream & document_input);
-
-  const vector<pair<size_t, size_t>> Lookup(const string& word) const;
-
-  const string& GetDocsCount() const {
-    return docs_count;
+  const string& GetDocument(size_t id) const {
+    return docs[id];
   }
 
 private:
-  size_t docs_count = 0;
   map<string, vector<pair<size_t,size_t>>> index;
+  vector<string> docs;
 };
 
 class SearchServer {
 public:
   SearchServer() = default;
-
-  explicit SearchServer(istream& document_input)
-      : index(InvertedIndex(document_input)) {}
-
+  explicit SearchServer(istream& document_input);
   void UpdateDocumentBase(istream& document_input);
-
   void AddQueriesStream(istream& query_input, ostream& search_results_output);
 
 private:
-  Synchronized<InvertedIndex> index;
-  vector<future<void>> futures;
+  InvertedIndex index;
 };
-
