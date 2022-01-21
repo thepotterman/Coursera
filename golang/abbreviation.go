65f40abcdb41b4c1986fe66dd9d6682b9c5bded9
Vladimir Polikushin <p-vov4ik@yandex.ru>
2022-01-21 15:58:41 +0300

    add some solutions

---
+++ golang/abbreviation.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	phrase := readString()
+
+	// 1. Разбейте фразу на слова, используя `strings.Fields()`
+	// 2. Возьмите первую букву каждого слова и приведите
+	//    ее к верхнему регистру через `unicode.ToUpper()`
+	// 3. Если слово начинается не с буквы, игнорируйте его
+	//    проверяйте через `unicode.IsLetter()`
+	// 4. Составьте слово из получившихся букв и запишите его
+	//    в переменную `abbr`
+    // ...
+
+    var abbr string
+
+    for _, word := range strings.Fields(phrase) {
+        runes_word := []rune(word)
+        is_letter := unicode.ToUpper(runes_word[0])
+        if unicode.IsLetter(is_letter) {
+            abbr += string(is_letter)
+        }
+    }
+
+	fmt.Println(string(abbr))
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readString читает строку из `os.Stdin` и возвращает ее
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
