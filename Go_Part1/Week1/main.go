7c4ea8260348038078d24a27a31118e961452b55
Vladimir Polikushin <p-vov4ik@yandex.ru>
2022-04-04 11:42:48 +0300

    FINALLY GOT FIRST PROGRAM

--- Go_Part1/Week1/main.go
+++ Go_Part1/Week1/main.go
@@ -2,11 +2,67 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"io"
 	"os"
-	"path/filepath"
-	"strings"
+    "strconv"
+	//"path/filepath"
+	//"strings"
 )
+
+func formatSize(size_raw int64) string {
+    if size_raw == 0 {
+        return "(empty)"
+    } else {
+        return "(" + strconv.FormatInt(size_raw, 10) + "b)"
+    }
+}
+
+func dirTree(out io.Writer, path string, printFiles bool) error {
+    err := dirTreeReal(out, path, printFiles, "")
+    return err
+}
+
+func dirTreeReal(out io.Writer, path string, printFiles bool, prefix string) error {
+    currentPrefix := prefix + "├───"
+    file, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    dirEntry, err := file.ReadDir(0)
+    if err != nil {
+        return err
+    }
+    sort.Slice(dirEntry, func(lhs, rhs int) bool {
+        return dirEntry[lhs].Name() < dirEntry[rhs].Name()
+    })
+    if(!printFiles) {
+        var dirEntryNew []os.DirEntry
+        for _, file := range dirEntry {
+            if file.IsDir() {
+                dirEntryNew = append(dirEntryNew, file)
+            }
+        }
+        dirEntry = dirEntryNew
+    }
+    for idx, file := range dirEntry {
+        if idx == len(dirEntry) - 1 {
+            currentPrefix = prefix + "└───"
+        }
+        if printFiles && !file.IsDir() {
+            info, _ := file.Info()
+            fmt.Fprintln(out, currentPrefix + file.Name(), formatSize(info.Size()))
+        } else if file.IsDir() {
+            fmt.Fprintln(out, currentPrefix + file.Name())
+            if idx == len(dirEntry) - 1 {
+                dirTreeReal(out, path + "/" + file.Name(), printFiles, prefix + "\t")
+            } else {
+                dirTreeReal(out, path + "/" + file.Name(), printFiles, prefix + "│" + "\t")
+            }
+        }
+    }
+    return nil
+}
 
 func main() {
 	out := os.Stdout
